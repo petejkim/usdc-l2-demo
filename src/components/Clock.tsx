42a1aaa08107d70c920e72aba1b9c14c74b3789b
petejkim <petejkim@users.noreply.github.com>
2020-09-17 12:17:52 -0700

    Toggle between colors by clicking on the clock

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,9 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { formatTime } from "../util/types";
 import "./Clock.scss";
 
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+
 export function Clock(): JSX.Element {
   const [date, setDate] = useState(new Date());
+
+  const click = useCallback((evt: React.MouseEvent) => {
+    evt.preventDefault();
+    document.body.classList?.toggle("alt-color");
+  }, []);
 
   useEffect(() => {
     const timer = window.setTimeout(() => {
@@ -13,7 +21,7 @@ export function Clock(): JSX.Element {
   }, [date]);
 
   return (
-    <div className="Clock">
+    <div className="Clock" onClick={click}>
       <div className="Clock-time">{formatTime(date)}</div>
       <div className="Clock-nosleep">Ethereum Never Sleeps</div>
     </div>
